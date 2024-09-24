7a08652e82721b12a282f8a6b8733edca0ad0fe7
fwidio <feliciya.widio@contracted.sampoerna.com>
2024-09-25 01:07:47 +0700

    streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -38,7 +38,7 @@ with col1:
     # Selection of type of data
     data_type = st.selectbox("Select the type of data", ["Pulse Survey", "CSAT Feedback", "Lifecycle Applying Challenge", "Contact Center"])
 with col2:
-    comments_file = st.file_uploader("Upload your input file (.xlsx)", type="xlsx")
+    comments_file_path = r"C:\Users\fwidio\Downloads\train_csatAug24_new.xlsx"
 
 if comments_file:
     # Insert the database inside the comment since I have a file called master database
